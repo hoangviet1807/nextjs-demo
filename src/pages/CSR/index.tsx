1dae55143cedc8550a843d28ead17777e8dfb67d
viet bui <hoangviet1807@gmail.com>
2023-07-08 20:35:44 +0700

    second-commit

--- src/pages/CSR/index.tsx
+++ src/pages/CSR/index.tsx
@@ -13,13 +13,6 @@ export default function CSR() {
 
     return (
         <>
-            <Head>
-                <title>Test1</title>
-                <meta
-                    name="description"
-                    content="Learn how to build a personal website using Next.js"
-                />
-            </Head>
             <div className="py-6 px-6">
                 <h1 className="text-[48px]">HEADER</h1>
                 {!data ? <div className="text-[30px] text-red-600">...Loading</div > : <div>{JSON.stringify(data)}</div>}
